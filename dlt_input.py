f82481f3ad7b1fdae1a29de5c9c417c51baab5d9
samantaray.anil@aol.com <samantaray.anil@aol.com>
2025-09-27 16:47:27 +0000

    dlt

--- dlt_input.py
+++ dlt_input.py
@@ -15,6 +15,11 @@ import dlt
 
 # COMMAND ----------
 
+_order_status = spark.conf.get("order_status")
+
+# COMMAND ----------
+
+#Streaming table used check point to ingest data incrementaly.
 @dlt.table(
   table_properties={"quality" : "bronze"},
   comment = "Order bronze table"
@@ -46,11 +51,12 @@ def orders_autoloader_bronze():
         spark
         .readStream
         .format("cloudFiles")
+        .option("skipChangeCommits", "true")
         .option("cloudFiles.format", "csv")
         .option("pathGlobFilter", "*.csv")
         .option("header", "true")
         .option("cloudFiles.schemaHints", "o_orderkey long, o_custkey long, o_orderstatus string, o_totalprice decimal(18,2), o_orderdate date, o_orderpriority string, o_clerk string, o_shippriority integer, o_comment string")
-        .option("cloudFiles.schemaLocation", "/Volumes/dev/etl/landing/autoloader/schemas/1/")
+        .option("cloudFiles.schemaLocation", "/Volumes/dev/etl/landing/autoloader/schemas/")
         .option("cloudFiles.schemaEvolutionMode", "none")
         .load("/Volumes/dev/etl/landing/files/")
     )
@@ -67,7 +73,7 @@ dlt.create_streaming_table("orders_union_bronze")
 )
 def orders_delta_append():
     return(
-        spark.readStream.table("LIVE.orders_bronze")
+        spark.readStream.option("skipChangeCommits", "true").table("LIVE.orders_bronze")
     )
 
 #Append autoloader
@@ -76,7 +82,7 @@ def orders_delta_append():
 )
 def orders_autooader_append():
     return(
-        spark.readStream.table("LIVE.orders_autoloader_bronze")
+        spark.readStream.option("skipChangeCommits", "true").table("LIVE.orders_autoloader_bronze")
     )
 
 # COMMAND ----------
@@ -90,7 +96,7 @@ def joined_vw():
 
 # COMMAND ----------
 
-from pyspark.sql.functions import current_timestamp, count
+from pyspark.sql.functions import current_timestamp, count, col
 @dlt.table(
   table_properties={"quality" : "silver"},
   comment = "Joined view",
@@ -109,6 +115,21 @@ def joined_silver():
 )
 def joined_gold():
     df = spark.read.table("LIVE.joined_silver")\
-        .groupBy("c_mktsegment").agg(count("o_orderkey").alias("sum_orders"))\
+        .groupBy("c_mktsegment").agg(count("o_orderkey").alias("count_orders"))\
         .withColumn("__insert_date", current_timestamp())
     return df 
+
+# COMMAND ----------
+
+for status in _order_status.split(","):
+    @dlt.table(
+    table_properties={"quality" : "gold"},
+    comment = "Orders aggregated gold",
+    name = f"orders_agg_{status}_gold"
+    )
+    def joined_gold():
+        df = spark.read.table("LIVE.joined_silver")\
+            .filter(f"o_orderstatus = '{status}'")\
+            .groupBy("c_mktsegment").agg(count("o_orderkey").alias("count_orders"))\
+            .withColumn("__insert_date", current_timestamp())
+        return df 
